31a67348372d9d14ec7ba97e9eb401c145880735
Abdoulkader MOUSSA MOHAMED <abdoulkader.moussa-mohamed@etudiant.univ-rennes1.fr>
2023-04-02 16:07:06 +0200

    Challenge4: first push

--- isos_inject.c
+++ isos_inject.c
@@ -115,7 +115,46 @@ int main(int argc, char **argv) {
          arguments.elf_file_to_analyze);
   }
 
-  int index_pt_note = get_index_of_pt_note_program_header(fd);
+  /*--   CHALLENGE 2   --*/
+
+  /* To get informations on the binary like his size */
+  struct stat fstat_structure;
+  if (fstat(fd, &fstat_structure) == -1) {
+    close(fd);
+    perror("fstat");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Mapping between a process address space and the binary. Binary can now be
+   * accessed like an array. */
+  char *addr = mmap(0, fstat_structure.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (addr == MAP_FAILED) {
+    close(fd);
+    perror("mmap");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Initializing executable header structure */
+  Elf64_Ehdr executable_header;
+  memcpy(&executable_header, addr, sizeof(Elf64_Ehdr));
+  fprintf(stdout, "Number of program headers: %d. \n",
+          executable_header.e_phnum);
+
+  int index_pt_note = -1; /* first program header of type PT_NOTE index */
+  for (int i = 0; i < executable_header.e_phnum; i++) {
+
+    /* Initializing of program header number i */
+    Elf64_Phdr program_header;
+    memcpy(&program_header,
+           addr + executable_header.e_phoff + i * sizeof(Elf64_Phdr),
+           sizeof(Elf64_Phdr));
+
+    if (program_header.p_type == PT_NOTE) {
+      index_pt_note = i;
+      break;
+    }
+  }
+
   if (index_pt_note == -1) {
     close(fd);
     errx(EXIT_FAILURE, "Any program header of type PT_NOTE found in %s\n",
@@ -125,6 +164,8 @@ int main(int argc, char **argv) {
     fprintf(stdout, "Index of first program header of type PT_NOTE: %d\n",
             index_pt_note);
   }
+
+  /*--   CHALLENGE3   --*/
 
   int inject_file_fd = open(arguments.code_to_inject, O_RDONLY);
   if (inject_file_fd == -1) {
@@ -148,6 +189,7 @@ int main(int argc, char **argv) {
   /* Code injection at the end of the binary */
   read(inject_file_fd, buffer, fstat_inject.st_size);
   write(fd, buffer, fstat_inject.st_size);
+  free(buffer);
 
   /* Computing base address so that the difference with the offset become
     zero modulo 4096.*/
@@ -155,6 +197,48 @@ int main(int argc, char **argv) {
       (end_position_elf - arguments.injected_code_base_address) % ALIGNMENT;
 
   printf("Computed base address: %d\n", arguments.injected_code_base_address);
+
+  /*--   CHALLENGE4   --*/
+
+  Elf64_Shdr *section_headers =
+      (Elf64_Shdr *)malloc(executable_header.e_shnum * sizeof(Elf64_Shdr));
+  if (section_headers == NULL) {
+    errx(EXIT_FAILURE, "Error while calling malloc ");
+  }
+
+  memcpy(section_headers, addr + executable_header.e_shoff,
+         executable_header.e_shnum * sizeof(Elf64_Shdr));
+
+  int index_of_shstrtab = executable_header.e_shstrndx;
+  printf("Index of section header '.shstrtab' %d \n", index_of_shstrtab);
+
+  int index_of_note_abi_tag = -1;
+  for (int i = 0; i < executable_header.e_shnum; i++) {
+    char *sh_name =
+        (char *)(addr + section_headers[index_of_shstrtab].sh_offset +
+                 section_headers[i].sh_name); /* Section header name */
+
+    if (strcmp(".note.ABI-tag", sh_name) == 0) {
+      printf("Index of section header '.note.ABI-tag' %d \n", i);
+      index_of_note_abi_tag = i;
+
+      section_headers[i].sh_type = SHT_PROGBITS;
+      section_headers[i].sh_addr = arguments.injected_code_base_address;
+      section_headers[i].sh_offset = end_position_elf;
+      section_headers[i].sh_size = fstat_inject.st_size;
+      section_headers[i].sh_addralign = 16;
+      section_headers[i].sh_flags = SHF_EXECINSTR;
+
+      break;
+    }
+  }
+
+  lseek(fd,
+        executable_header.e_shoff +
+            (index_of_note_abi_tag * sizeof(Elf64_Shdr)),
+        SEEK_SET);
+  write(fd, &section_headers[index_of_note_abi_tag], sizeof(Elf64_Shdr));
+  free(section_headers);
 
   close(fd);
   close(inject_file_fd);
--- isos_inject.h
+++ isos_inject.h
@@ -1,34 +1,47 @@
 #pragma once
 #include <stdint.h>
 
-#define PT_NOTE 4 /* Auxiliary information */
+#define PT_NOTE 4              /* Auxiliary information */
+#define SHT_PROGBITS 1         /* Program data */
+#define SHF_EXECINSTR (1 << 2) /* Executable */
 
-typedef struct
-{
-  unsigned char	e_ident[16];	/* Magic number and other info */
-  uint16_t	e_type;			/* Object file type */
-  uint16_t	e_machine;		/* Architecture */
-  uint32_t	e_version;		/* Object file version */
-  uint64_t	e_entry;		/* Entry point virtual address */
-  uint64_t	e_phoff;		/* Program header table file offset */
-  uint64_t	e_shoff;		/* Section header table file offset */
-  uint32_t	e_flags;		/* Processor-specific flags */
-  uint16_t	e_ehsize;		/* ELF header size in bytes */
-  uint16_t	e_phentsize;		/* Program header table entry size */
-  uint16_t	e_phnum;		/* Program header table entry count */
-  uint16_t	e_shentsize;		/* Section header table entry size */
-  uint16_t	e_shnum;		/* Section header table entry count */
-  uint16_t	e_shstrndx;		/* Section header string table index */
+typedef struct {
+  unsigned char e_ident[16]; /* Magic number and other info */
+  uint16_t e_type;           /* Object file type */
+  uint16_t e_machine;        /* Architecture */
+  uint32_t e_version;        /* Object file version */
+  uint64_t e_entry;          /* Entry point virtual address */
+  uint64_t e_phoff;          /* Program header table file offset */
+  uint64_t e_shoff;          /* Section header table file offset */
+  uint32_t e_flags;          /* Processor-specific flags */
+  uint16_t e_ehsize;         /* ELF header size in bytes */
+  uint16_t e_phentsize;      /* Program header table entry size */
+  uint16_t e_phnum;          /* Program header table entry count */
+  uint16_t e_shentsize;      /* Section header table entry size */
+  uint16_t e_shnum;          /* Section header table entry count */
+  uint16_t e_shstrndx;       /* Section header string table index */
 } Elf64_Ehdr;
 
-typedef struct 
-{
-  uint32_t	p_type;			/* Segment type */
-  uint32_t	p_flags;		/* Segment flags */
-  uint64_t	p_offset;		/* Segment file offset */
-  uint64_t	p_vaddr;		/* Segment virtual address */
-  uint64_t	p_paddr;		/* Segment physical address */
-  uint64_t	p_filesz;		/* Segment size in file */
-  uint64_t	p_memsz;		/* Segment size in memory */
-  uint64_t	p_align;		/* Segment alignment */
+typedef struct {
+  uint32_t p_type;   /* Segment type */
+  uint32_t p_flags;  /* Segment flags */
+  uint64_t p_offset; /* Segment file offset */
+  uint64_t p_vaddr;  /* Segment virtual address */
+  uint64_t p_paddr;  /* Segment physical address */
+  uint64_t p_filesz; /* Segment size in file */
+  uint64_t p_memsz;  /* Segment size in memory */
+  uint64_t p_align;  /* Segment alignment */
 } Elf64_Phdr;
+
+typedef struct {
+  uint32_t sh_name;      /* section name (string tbl index)*/
+  uint32_t sh_type;      /* section type*/
+  uint64_t sh_flags;     /* section flags*/
+  uint64_t sh_addr;      /* section virtual addr at execution*/
+  uint64_t sh_offset;    /* section file offset*/
+  uint64_t sh_size;      /* section size in bytes*/
+  uint32_t sh_link;      /* link to another section*/
+  uint32_t sh_info;      /* additional section information */
+  uint64_t sh_addralign; /* section alignement*/
+  uint64_t sh_entsize;   /* entry size if section holds table*/
+} Elf64_Shdr;
