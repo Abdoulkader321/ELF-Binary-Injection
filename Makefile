15d8183b50451c9d67d6247349f3866856847072
Abdoulkader MOUSSA MOHAMED <abdoulkader.moussa-mohamed@etudiant.univ-rennes1.fr>
2023-04-19 18:51:59 +0200

    Challenge 7: Update

--- Makefile
+++ Makefile
@@ -18,7 +18,8 @@ inject_code_for_got_plt_hijack: inject_code_for_got_plt_hijack.asm
 
 check_warnings:
 	make clean;
-	make inject_code;
+	make inject_code_for_entry_point_modification;
+	make inject_code_for_got_plt_hijack;
 	clang -fsyntax-only -Wall -Wextra -Wuninitialized -Wpointer-arith -Wcast-qual -Wcast-align isos_inject.c argp_parser.c;
 	gcc -O2 -Warray-bounds -Wsequence-point -Walloc-zero -Wnull-dereference -Wpointer-arith -Wcast-qual -Wcast-align=strict -o binaries/isos_inject-1 isos_inject.c -lbfd;
 	gcc -fanalyzer isos_inject.c -lbfd -o binaries/isos_inject-2;
--- isos_inject.c
+++ isos_inject.c
@@ -51,46 +51,101 @@ void check_binary_with_libbfd(struct arguments *arguments) {
   bfd_close(bfd_file);
 }
 
-int get_index_of_pt_note_program_header(int fd) {
-  /* To get informations on the binary like his size */
-  struct stat fstat_structure;
-  if (fstat(fd, &fstat_structure) == -1) {
+int get_index_of_first_program_header(Elf64_Ehdr executable_header,
+                                      char **addr) {
+
+  int index_pt_note = -1;
+  /* Initializing of program header number i */
+  Elf64_Phdr *program_header =
+      (Elf64_Phdr *)malloc(executable_header.e_phnum * sizeof(Elf64_Phdr));
+  ;
+  memcpy(program_header, *addr + executable_header.e_phoff,
+         executable_header.e_phnum * sizeof(Elf64_Phdr));
+
+  for (int i = 0; i < executable_header.e_phnum; i++) {
+
+    if (program_header[i].p_type == PT_NOTE) {
+      index_pt_note = i;
+      break;
+    }
+  }
+  free(program_header);
+
+  return index_pt_note;
+}
+
+int append_inject_code_to_elf_and_compute_new_base_address(
+    int fd, struct arguments *arguments) {
+
+  int inject_file_fd = open(arguments->code_to_inject, O_RDONLY);
+  if (inject_file_fd == -1) {
+    errx(EXIT_FAILURE, "Error while opening the file %s .",
+         arguments->code_to_inject);
+  }
+
+  /* To get informations on the inject_code like his size */
+  struct stat fstat_inject;
+  if (fstat(inject_file_fd, &fstat_inject) == -1) {
     close(fd);
     perror("fstat");
     exit(EXIT_FAILURE);
   }
 
-  /* Mapping between a process address space and the binary. Binary can now be
-   * accessed like an array. */
-  char *addr = mmap(0, fstat_structure.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-  if (addr == MAP_FAILED) {
-    close(fd);
-    perror("mmap");
-    exit(EXIT_FAILURE);
-  }
+  int end_position_elf = lseek(fd, 0, SEEK_END); /* At the end of the binary */
 
-  /* Initializing executable header structure */
-  Elf64_Ehdr executable_header;
-  memcpy(&executable_header, addr, sizeof(Elf64_Ehdr));
-  fprintf(stdout, "Number of program headers: %d. \n",
-          executable_header.e_phnum);
+  /* Buffer containing the injection code bytes */
+  char *buffer = malloc(fstat_inject.st_size * sizeof(char));
 
-  int index_pt_note = -1; /* first program header of type PT_NOTE index */
-  for (int i = 0; i < executable_header.e_phnum; i++) {
+  /* Code injection at the end of the binary */
+  read(inject_file_fd, buffer, fstat_inject.st_size);
+  write(fd, buffer, fstat_inject.st_size);
+  free(buffer);
+  close(inject_file_fd);
 
-    /* Initializing of program header number i */
-    Elf64_Phdr program_header;
-    memcpy(&program_header,
-           addr + executable_header.e_phoff + i * sizeof(Elf64_Phdr),
-           sizeof(Elf64_Phdr));
+  /* Computing base address so that the difference with the offset become
+    zero modulo 4096.*/
+  arguments->injected_code_base_address +=
+      (end_position_elf - arguments->injected_code_base_address) % ALIGNMENT;
 
-    if (program_header.p_type == PT_NOTE) {
-      index_pt_note = i;
+  printf("Computed base address: %d\n", arguments->injected_code_base_address);
+
+  return end_position_elf;
+}
+
+int get_index_note_abi_tag_section_header(Elf64_Shdr *section_headers,
+                                          Elf64_Ehdr executable_header,
+                                          char *addr) {
+  int index_of_note_abi_tag = -1;
+
+  for (int i = 0; i < executable_header.e_shnum; i++) {
+    char *sh_name =
+        (char *)(addr +
+                 section_headers[executable_header.e_shstrndx].sh_offset +
+                 section_headers[i].sh_name); /* Section header name */
+
+    if (strcmp(".note.ABI-tag", sh_name) == 0) {
+      printf("Index of section header '.note.ABI-tag' %d \n", i);
+      index_of_note_abi_tag = i;
+
       break;
     }
   }
 
-  return index_pt_note;
+  return index_of_note_abi_tag;
+}
+
+void overwrite_pt_note_program_header(Elf64_Phdr *pt_note_ph,
+                                      Elf64_Shdr *section_headers,
+                                      int index_of_note_abi_tag) {
+  pt_note_ph->p_type = PT_LOAD;
+  pt_note_ph->p_flags |= PF_X;
+  pt_note_ph->p_align = 0x1000;
+
+  pt_note_ph->p_offset = section_headers[index_of_note_abi_tag].sh_offset;
+  pt_note_ph->p_paddr = section_headers[index_of_note_abi_tag].sh_addr;
+  pt_note_ph->p_vaddr = section_headers[index_of_note_abi_tag].sh_addr;
+  pt_note_ph->p_filesz = section_headers[index_of_note_abi_tag].sh_size;
+  pt_note_ph->p_memsz = section_headers[index_of_note_abi_tag].sh_size;
 }
 
 /**
@@ -178,6 +233,8 @@ static struct argp argp = {.options = options,
 
 int main(int argc, char **argv) {
 
+  /*--   CHALLENGE 1   --*/
+
   struct arguments arguments;
   arguments.argCount = 0;
   arguments.modify_entry_function_address = false;
@@ -220,20 +277,9 @@ int main(int argc, char **argv) {
   fprintf(stdout, "Number of program headers: %d. \n",
           executable_header.e_phnum);
 
-  int index_pt_note = -1; /* first program header of type PT_NOTE index */
-  for (int i = 0; i < executable_header.e_phnum; i++) {
-
-    /* Initializing of program header number i */
-    Elf64_Phdr program_header;
-    memcpy(&program_header,
-           addr + executable_header.e_phoff + i * sizeof(Elf64_Phdr),
-           sizeof(Elf64_Phdr));
-
-    if (program_header.p_type == PT_NOTE) {
-      index_pt_note = i;
-      break;
-    }
-  }
+  int index_pt_note = get_index_of_first_program_header(
+      executable_header,
+      &addr); /* first program header of type PT_NOTE index */
 
   if (index_pt_note == -1) {
     close(fd);
@@ -247,36 +293,8 @@ int main(int argc, char **argv) {
 
   /*--   CHALLENGE3   --*/
 
-  int inject_file_fd = open(arguments.code_to_inject, O_RDONLY);
-  if (inject_file_fd == -1) {
-    errx(EXIT_FAILURE, "Error while opening the file %s .",
-         arguments.code_to_inject);
-  }
-
-  /* To get informations on the inject_code like his size */
-  struct stat fstat_inject;
-  if (fstat(inject_file_fd, &fstat_inject) == -1) {
-    close(fd);
-    perror("fstat");
-    exit(EXIT_FAILURE);
-  }
-
-  int end_position_elf = lseek(fd, 0, SEEK_END); /* At the end of the binary */
-
-  /* Buffer containing the injection code bytes */
-  char *buffer = malloc(fstat_inject.st_size * sizeof(char));
-
-  /* Code injection at the end of the binary */
-  read(inject_file_fd, buffer, fstat_inject.st_size);
-  write(fd, buffer, fstat_inject.st_size);
-  free(buffer);
-
-  /* Computing base address so that the difference with the offset become
-    zero modulo 4096.*/
-  arguments.injected_code_base_address +=
-      (end_position_elf - arguments.injected_code_base_address) % ALIGNMENT;
-
-  printf("Computed base address: %d\n", arguments.injected_code_base_address);
+  int inject_code_size =
+      append_inject_code_to_elf_and_compute_new_base_address(fd, &arguments);
 
   /*--   CHALLENGE4   --*/
 
@@ -289,40 +307,25 @@ int main(int argc, char **argv) {
   memcpy(section_headers, addr + executable_header.e_shoff,
          executable_header.e_shnum * sizeof(Elf64_Shdr));
 
-  int index_of_shstrtab = executable_header.e_shstrndx;
-  printf("Index of section header '.shstrtab' %d \n", index_of_shstrtab);
-
-  int index_of_note_abi_tag = -1;
-
-  for (int i = 0; i < executable_header.e_shnum; i++) {
-    char *sh_name =
-        (char *)(addr + section_headers[index_of_shstrtab].sh_offset +
-                 section_headers[i].sh_name); /* Section header name */
-
-    if (strcmp(".note.ABI-tag", sh_name) == 0) {
-      printf("Index of section header '.note.ABI-tag' %d \n", i);
-      index_of_note_abi_tag = i;
-
-      section_headers[i].sh_type = SHT_PROGBITS;
-      section_headers[i].sh_addr = arguments.injected_code_base_address;
-      section_headers[i].sh_offset = end_position_elf;
-      section_headers[i].sh_size = fstat_inject.st_size;
-      section_headers[i].sh_addralign = ADDR_ALIGN;
-      section_headers[i].sh_flags |= SHF_EXECINSTR;
-      
-      break;
-    }
-  }
+  int index_of_note_abi_tag = get_index_note_abi_tag_section_header(
+      section_headers, executable_header, addr);
 
   if (index_of_note_abi_tag == -1) {
     /* Section header called '.note.ABI-tag' not found */
 
     free(section_headers);
     close(fd);
-    close(inject_file_fd);
 
     errx(EXIT_FAILURE, " '.note.ABI-tag' not found in section headers\n");
   }
+
+  section_headers[index_of_note_abi_tag].sh_type = SHT_PROGBITS;
+  section_headers[index_of_note_abi_tag].sh_addr =
+      arguments.injected_code_base_address;
+  section_headers[index_of_note_abi_tag].sh_offset = fstat_structure.st_size;
+  section_headers[index_of_note_abi_tag].sh_size = inject_code_size;
+  section_headers[index_of_note_abi_tag].sh_addralign = ADDR_ALIGN;
+  section_headers[index_of_note_abi_tag].sh_flags |= SHF_EXECINSTR;
 
   lseek(fd,
         executable_header.e_shoff +
@@ -342,7 +345,7 @@ int main(int argc, char **argv) {
 
   /* Set the name of the injected section*/
   lseek(fd,
-        section_headers[index_of_shstrtab].sh_offset +
+        section_headers[executable_header.e_shstrndx].sh_offset +
             section_headers[index_of_note_abi_tag].sh_name,
         SEEK_SET);
 
@@ -356,16 +359,10 @@ int main(int argc, char **argv) {
          addr + executable_header.e_phoff + index_pt_note * sizeof(Elf64_Phdr),
          sizeof(Elf64_Phdr));
 
-  pt_note_ph.p_type = PT_LOAD;
-  pt_note_ph.p_flags |= PF_X;
-  pt_note_ph.p_align = 0x1000;
+  overwrite_pt_note_program_header(&pt_note_ph, section_headers,
+                                   index_of_note_abi_tag);
 
-  pt_note_ph.p_offset = section_headers[index_of_note_abi_tag].sh_offset;
-  pt_note_ph.p_paddr = section_headers[index_of_note_abi_tag].sh_addr;
-  pt_note_ph.p_vaddr = section_headers[index_of_note_abi_tag].sh_addr;
-  pt_note_ph.p_filesz = fstat_inject.st_size;
-  pt_note_ph.p_memsz = section_headers[index_of_note_abi_tag].sh_size;
-
+  /* Save the updated program header to file */
   lseek(fd, executable_header.e_phoff + index_pt_note * sizeof(Elf64_Phdr),
         SEEK_SET);
 
@@ -389,9 +386,7 @@ int main(int argc, char **argv) {
   }
 
   free(section_headers);
-
   close(fd);
-  close(inject_file_fd);
 
   return EXIT_SUCCESS;
 }
